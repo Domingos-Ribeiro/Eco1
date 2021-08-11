fd1a400f700413b808fc6259f83765c89cc38762
Domingos Ribeiro <domingos.ribeiro@gmail.com>
2021-08-11 15:20:18 +0100

    Add project files.

---
+++ Controllers/AController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eco1.Controllers
+{
+    public class AController : Controller
+    {
+        EconomatoEntities db = new EconomatoEntities();
+        // GET: A
+
+
+        //Listagem
+        public ActionResult Index()
+        {
+            return View(db.Entidades.ToList());
+        }
+
+        //Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Entidades());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Entidades entidades)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entidades.Add(entidades);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(entidades);
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Eco1.MvcApplication" Language="C#" %>
